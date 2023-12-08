ab8f4bd6a6372802aea18516b49abb3940fa16fa
ajaysaini001 <123984045+ajaysaini001@users.noreply.github.com>
2023-12-08 21:00:43 +0530

    first commit

---
+++ src/components/Clent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Clent = () => {
+  return (
+    <div>Clent</div>
+  )
+}
+
+export default Clent
---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+
+import Clent from "./Clent";
+import Billing from "./Billing";
+import Business from "./Business";
+import CTA from "./CTA";
+import CardDeal from "./CardDeal";
+import Navbar from "./Navbar";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+import Fotter from "./Fotter";
+import Stats from "./Stats";
+
+export {
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    Clent,
+    CTA,
+    Stats,
+    Fotter,
+    Testimonials,
+    Hero,
+}
